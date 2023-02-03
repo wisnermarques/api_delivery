e3942f227a895758029edeb8077265937173b6e2
Wisner Marques <wisner.a.marques@gmail.com>
2023-02-03 11:45:19 -0300

    bd

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'svc-app-loja.mysql.database.azure.com'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'bd_app_delivery'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'root'),
-      ssl: env.bool('DATABASE_SSL', false),
+      user: env('DATABASE_USERNAME', 'user_bdloja'),
+      password: env('DATABASE_PASSWORD', 'Waslote@80'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
