c4dfbb77809713d6d9c27876fe7df6740c8abd54
Wisner Marques <wisner.a.marques@gmail.com>
2023-02-07 09:32:48 -0300

    database production

--- config/database.js
+++ config/database.js
@@ -6,8 +6,25 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'bd_app_delivery'),
       user: env('DATABASE_USERNAME', 'user_bdloja'),
-      password: env('DATABASE_PASSWORD', 'Waslote@80'),
-      ssl: env.bool('DATABASE_SSL', true),
+      password: env('DATABASE_PASSWORD', 'Senac@2022'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'bd_app_delivery'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', 'root'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
