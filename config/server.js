b741cad20281e8318544e640367a2691aacddc8a
Wisner Marques <wisner.a.marques@gmail.com>
2023-02-15 16:16:55 -0300

    gitignore

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   //port: env.int('PORT', 1337),
-  //port: env.int('PORT', 8080),
-  port: process.env.PORT || 8080,
+  port: env.int('PORT', 8080),
+  
   // app: {
   //   keys: env.array('APP_KEYS'),
   // },
