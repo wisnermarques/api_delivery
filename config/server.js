b5c24c98ca543150bf07abab04d405205d931691
Wisner Marques <wisner.a.marques@gmail.com>
2023-02-15 15:56:47 -0300

    server

--- config/server.js
+++ config/server.js
@@ -2,7 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   //port: env.int('PORT', 1337),
   port: env.int('PORT', 8080),
+  // app: {
+  //   keys: env.array('APP_KEYS'),
+  // },
   app: {
-    keys: env.array('APP_KEYS'),
-  },
+    keys: ['128FxhvEXlPD+NcumnKz3Q==,qu50S9TrMff6ggo7INCgZg==,YXA7yE/phbTOclq9YhfvEA==,UMDenk/78tc4JRPNqYN+rA==']
+  }
 });
